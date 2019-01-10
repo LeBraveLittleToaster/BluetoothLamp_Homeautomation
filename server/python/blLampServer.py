09b5c65ea36ef8265b0d2eb0fc87d7fb771d1710
Pascal Schiessle <pascal.schiessle@web.de>
2019-01-10 14:54:00 +0100

    Hello world should work

--- server/python/blLampServer.py
+++ server/python/blLampServer.py
@@ -1,9 +1,15 @@
 import bluetooth
 import sys
+import struct
 import getopt
 
 allowed_devices = ["98:D3:31:FC:79:0C"]
 port = 1
+
+class DataStream(bytearray):
+
+    def append(self, v, fmt='>B'):
+        self.extend(struct.pack(fmt, v))
 
 def receiveMessages():
   server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -21,11 +27,16 @@ def receiveMessages():
   client_sock.close()
   server_sock.close()
   
-def sendMessageTo(targetBluetoothMacAddress):
+def sendMessageTo(targetBluetoothMacAddress,r,g,b):
+  print "Sending R=" + r + " G=" + g + " B=" + b
+  x = DataStream()
+  x.append(r)
+  x.append(g)
+  x.append(b)
   port = 1
   sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
   sock.connect((targetBluetoothMacAddress, port))
-  sock.send(sys.argv[0] + sys.argv[1] + sys.argv[2])
+  sock.send(x)
   sock.close()
   
 def lookUpNearbyBluetoothDevices():
@@ -36,9 +47,33 @@ def lookUpNearbyBluetoothDevices():
       print "Sending ->hello!!<-" + " [" + str(bdaddr) + "]"
       sendMessageTo(bdaddr)
 
-def sendToAllDevices():
+def sendToAllDevices(r,g,b):
   for address in allowed_devices:
     print str(bluetooth.lookup_name( address )) + " [" + str(address) + "]"
-    sendMessageTo(address)
+    sendMessageTo(address, r,g,b)
 
-sendToAllDevices()
+def parseArgs():
+  try:
+        opts, args = getopt.getopt(sys.argv[1:], "rgb", ["red", "green", "blue"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    r = None
+    g = None
+    b = None
+    verbose = False
+    for o, a in opts:
+        if o in ("-r", "--red"):
+          r = a
+        elif o in ("-g", "--green"):
+          g = a
+        elif o in ("-b", "--blue"):
+          b = a
+        else:
+            assert False, "unhandled option"
+
+  sendToAllDevices(r,g,b)
+
+parseArgs()
