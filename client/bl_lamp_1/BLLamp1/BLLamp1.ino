caa25a40995c195941266302b1f25c9d46a2692a
Pascal Schiessle <pascal.schiessle@web.de>
2019-01-10 17:29:35 +0100

    Hello world should work

--- client/bl_lamp_1/BLLamp1/BLLamp1.ino
+++ client/bl_lamp_1/BLLamp1/BLLamp1.ino
@@ -19,29 +19,27 @@ void loop() {
       Serial.print("V=");
       Serial.println(msgBuf);
       */
-      for(int i = 0; i < numberCount; i++){
-        int r = readValue();
-        Serial.println(r);
-        int g = readValue();
-        Serial.println(g);
-        int b = readValue();
-        Serial.println(b);
-      }
+      int r = readValue();
+      Serial.println(r);
+      int g = readValue();
+      Serial.println(g);
+      int b = readValue();
+      Serial.println(b);
     }
   }
 }
 int readValue(){
   Serial.print("START_");
-  int x1 = 48 - Serial.read();
+  int x1 = Serial.read() - 48;
   Serial.print(x1);
   Serial.print("_");
-  int x2 = 48 - Serial.read();
+  int x2 = Serial.read() - 48;
   Serial.print(x2);
   Serial.print("_");
-  int x3 = 48 - Serial.read();
+  int x3 = Serial.read() - 48;
   Serial.print(x3);
   Serial.print("_");
-  int x4 = 48 - Serial.read();
+  int x4 = Serial.read() - 48;
   Serial.print(x4);
   Serial.println("_END");
   return x1 + x2 + x3 + x4;
