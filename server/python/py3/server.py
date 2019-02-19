edcbf8501e6b0d2378a47322609d1a162ed69d5b
Pascal Schiessle <pascal.schiessle@web.de>
2019-02-19 12:32:31 +0100

    Added good encoding

--- server/python/py3/server.py
+++ server/python/py3/server.py
@@ -5,7 +5,7 @@ port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 while 1:
-    text = raw_input() # Note change to the old (Python 2) raw_input
+    text = "".join(map(chr, [112,52,52,250]))
     if text == "quit":
         break
     s.send(text)
