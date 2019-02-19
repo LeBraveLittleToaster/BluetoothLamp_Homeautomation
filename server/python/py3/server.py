ed709ec0136c7e5ad6f95239bddd4388aa97a6ec
Pascal Schiessle <pascal.schiessle@web.de>
2019-02-19 12:42:50 +0100

    First testserver implementation

--- server/python/py3/server.py
+++ server/python/py3/server.py
@@ -1,14 +1,23 @@
 import bluetooth
 import time
 
+v0 = [0,255,0,0]
+v1 = [255,0,0,1]
+v2 = [0,0,255,2]
+
 serverMACAddress = '98:d3:31:fd:89:ca'
 port = 1
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 while 1:
-    text = "".join(map(chr, [112,52,52,250]))
-    if text == "quit":
-        break
-    s.send(text)
-    time.sleep(.4);
+    time.sleep(5)
+    sendValues(v0)
+    time.sleep(5)
+    sendValues(v1)
+    time.sleep(5)
+    sendValues(v2)
 sock.close()
+
+def sendValues(values):
+    text = "#".join(map(chr, values))
+    s.send(text)
