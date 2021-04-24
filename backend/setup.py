24b251b62ea585217afd2797b4f690f7f38c4512
Pascal Schiessle <pascal.schiessle@web.de>
2021-04-24 03:29:51 +0200

    Added basic GUI, now connection has to follow

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='backend',
+    version='0.0.1',
+    packages=['utils'],
+    url='',
+    license='',
+    author='Pascal Schiessle',
+    author_email='pascal.schiessle@web.de',
+    description='Kivy Learning Project for Home Automation'
+)
--- backend/utils/Config.py
+++ backend/utils/Config.py
@@ -3,6 +3,17 @@ from enum import Enum
 from typing import List
 
 
+def auto_str(cls):
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+        )
+
+    cls.__str__ = __str__
+    return cls
+
+
 class ConType(Enum):
     BLUETOOTH = 0
     GPIO = 1
@@ -19,25 +30,26 @@ def parse_options_by_type(con_type: ConType, options: dict):
     return None
 
 
-def toStrip(json_strip: dict):
+def to_strip(strip_id: int, json_strip: dict):
     try:
         con_type = ConType[json_strip["con_type"]]
         options = parse_options_by_type(con_type, json_strip["options"])
-        return None if options is None else Strip(json_strip["name"], con_type, options)
+        return None if options is None else Strip(json_strip["name"], strip_id, con_type, options)
     except KeyError:
         return None
 
 
-def parseStrips(json_strips: dict):
+def parse_strips(json_strips: dict):
     if "strips" in json_strips and isinstance(json_strips["strips"], list):
-        return list(filter(lambda x: x is not None, map(lambda x: toStrip(x), json_strips["strips"])))
+        return list(filter(lambda x: x is not None,
+                           map(lambda i_x: to_strip(i_x[0], i_x[1]), enumerate(json_strips["strips"]))))
     else:
         raise Exception("Failed to parse config!")
 
 
 class Config:
     def __init__(self, json_config: dict):
-        self.strips: List[Strip] = parseStrips(json_config)
+        self.strips: List[Strip] = parse_strips(json_config)
 
 
 class ConOptions:
@@ -45,9 +57,12 @@ class ConOptions:
         pass
 
 
+@auto_str
 class Strip:
-    def __init__(self, name: str, con_type: ConType, options: ConOptions):
+
+    def __init__(self, name: str, strip_id: int, con_type: ConType, options: ConOptions):
         self.name = name
+        self.strip_id = strip_id
         self.con_type = con_type
         self.options = options
 
