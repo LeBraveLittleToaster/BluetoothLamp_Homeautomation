8074ec1883e0998430d56cfdaff5c5bcccc1d26f
Pascal Schiessle <pascal.schiessle@web.de>
2021-05-02 02:56:08 +0200

    Added a lot of fancy graphics

--- frontend/web/stripper-web/src/app/device-list/device-list.component.ts
+++ frontend/web/stripper-web/src/app/device-list/device-list.component.ts
@@ -1,3 +1,4 @@
+import { style, trigger, state, transition, animate } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -6,17 +7,49 @@ import Device from '../api-objects/device';
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.component.html',
-  styleUrls: ['./device-list.component.scss']
+  styleUrls: ['./device-list.component.scss'],
+  animations: [
+    trigger('fade', [      
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(".5s", style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        animate(".5s", style({opacity: 0}))
+      ])
+    ]),
+    trigger('fade_delayed', [      
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(".5s .5s", style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        animate(".5s .5s", style({opacity: 0}))
+      ])
+    ])
+  ],
 })
 export class DeviceListComponent implements OnInit {
 
   devices:Device[] | undefined = undefined
   color:string[] = []
+  isDetailOpen:boolean = false
+  selectedDevice:Device|undefined = undefined
 
   constructor(private http:HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.devices = this.route.snapshot.data.devices.data
+    console.log(this.devices)
+  }
+
+  onOnClicked(device:Device){
+    console.log("CLICKED")
+  }
+
+  selectDevice(device:Device|undefined){
+    this.selectedDevice = device
+    console.group(this.selectedDevice)
   }
 
   onSubmitClick(index:number):void{
--- services/stripper/net_messages/device_messages.py
+++ services/stripper/net_messages/device_messages.py
@@ -5,8 +5,13 @@ from stripper.model.strips import ControllableStrip
 
 
 def get_device_list_message(strips: List[ControllableStrip]) -> str:
+    strips = list(map(lambda x: x.to_dict(), strips))
+    strips_out = []
+    for i in range(0,3):
+        for s in strips:
+            strips_out.append(s)
     msg = {
         "m_type": "DEVICE_ALL",
-        "data": list(map(lambda x: x.to_dict(), strips))
+        "data": strips_out
     }
     return json.dumps(msg)
